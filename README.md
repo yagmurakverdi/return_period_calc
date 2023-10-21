ffb7e55dffec026981287d793b55cd422eed04f1
Yagmur Akverdi <yagmur.akverdi@boun.edu.tr>
2023-10-21 12:26:50 +0300

    [IMP] choose data file

--- README.md
+++ README.md
@@ -5,7 +5,13 @@ This is to setup, run and trace steps for Yagmur.
 
 Steps so far
 ------------
-* opened the Data file 
+* opened the Data file for 2026-2050 RCP 8.5 RFE data
+
+
+Data Set
+--------
+1. `Turkey_MPI_85_dn_SRF.2026-2050_pr_3hour.nc` - 25yr modeled 3hrs precipitation est. data
+2. `pr_2023_daily.nc` - daily modeled precipitation for 2023 - as sample
 
 
 Installing
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import sys
+
 import xarray as xr
 
+data_files = {
+    '26-50-8.5': 'Turkey_MPI_85_dn_SRF.2026-2050_pr_3hour.nc',
+    '22-23': 'pr_2023_daily.nc',
+}
 
-def open_data_file():
-    file_path = "./data/yagmur_Turkey_MPI_85_dn_SRF.2026-2050_pr_3hour.nc"
+data_path = './data/'
+
+
+def open_data_file(f):
+    if f in data_files:
+        file_path = data_path + data_files[f]
+    else:
+        print('choose a proper file', str(data_files))
+        return False
     nc_data = xr.open_dataset(file_path)
     return nc_data
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    nc_data = open_data_file()
-    # print('nc_data >>>')
+    nc_data = open_data_file('26-50-8.5')
+    if not nc_data:
+        print('error')
+        sys.exit(1)
     print(nc_data)
-    # print('===========')
